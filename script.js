d39545aed0968d2132314e0b31679fccbf00e832
ananay <i@ananayarora.com>
2016-09-07 04:07:12 +0530

    URL Formatting Added

--- script.js
+++ script.js
@@ -2,15 +2,30 @@
 * @Author: ananayarora
 * @Date:   2016-09-07T03:41:12+05:30
 * @Last modified by:   ananayarora
-* @Last modified time: 2016-09-07T03:48:06+05:30
+* @Last modified time: 2016-09-07T04:05:48+05:30
 */
 
 
 
 $(document).ready(function(){
     $(".check").click(function(){
-        $.getJSON("https://graph.facebook.com/"+$(".url").val(), function(r){
-            $(".num").html(r.share.share_count + " shares");
+        var prefix = 'http://';
+        var prefix2 = 'https://';
+        var url = $(".url").val();
+        if (url.substr(0, prefix.length) !== prefix && url.substr(0, prefix2.length) !== prefix2) {
+            url = prefix + url;
+        }
+        $.getJSON("https://graph.facebook.com/"+url, function(r){
+            if (r.share !== undefined) {
+                $(".num").html(r.share.share_count + " shares");
+            } else {
+                $(".num").html("0 shares");
+            }
         });
+    });
+    $(document).keydown(function(e){
+        if (e.keyCode == 13) {
+            $(".check").trigger('click');
+        }
     });
 });
